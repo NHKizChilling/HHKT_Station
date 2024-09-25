46659d4f7bd18d11710f407f590f0e3199ee3e55
NHKizChilling <114411322+NHKizChilling@users.noreply.github.com>
2024-09-25 19:38:09 +0700

    1st commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.hhkt_station {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.hhkt_station to javafx.fxml;
+    exports com.example.hhkt_station;
+}
